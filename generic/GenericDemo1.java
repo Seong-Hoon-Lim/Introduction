5e92c7143156053119854d675170f5583a07d9d4
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2022-07-12 16:34:50 +0900

    Add files via upload

---
+++ generic/GenericDemo1.java
@@ -0,0 +1,41 @@
+/* package org.opentutorials.javatutorials.generic;
+
+class StudentInfo{
+    public int grade;
+    StudentInfo(int grade){ this.grade = grade; }
+}
+class StudentPerson{
+    public StudentInfo info;
+    StudentPerson(StudentInfo info){ this.info = info; }
+}
+class EmployeeInfo{
+    public int rank;
+    EmployeeInfo(int rank){ this.rank = rank; }
+}
+class EmployeePerson{
+    public EmployeeInfo info;
+    EmployeePerson(EmployeeInfo info){ this.info = info; }
+}
+// 코드가 중복됨
+/*
+public class GenericDemo1 {
+    public static void main(String[] args) {
+        StudentInfo si = new StudentInfo(2);
+        StudentPerson sp = new StudentPerson(si);
+        System.out.println(sp.info.grade); // 2
+        EmployeeInfo ei = new EmployeeInfo(1);
+        EmployeePerson ep = new EmployeePerson(ei);
+        System.out.println(ep.info.rank); // 1
+        */
+/* public class GenericDemo1 {
+    public static void main(String[] args) {
+        Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
+        EmployeeInfo ei1 = p1.info;
+        System.out.println(ei1.rank); // 성공
+         
+        /*Person<String> p2 = new Person<String>("부장");
+        String ei2 = p2.info;
+        System.out.println(ei2.rank); //컴파일 실패
+	}
+
+} */
